26c0ae139ae9d6ec18f7f9189da8c71f326dc1b3
lucasfranson <lucasfranson27@gmail.com>
2020-12-15 15:20:21 +0000

    preparing the front

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />		
+		<link href="/css/default.css" rel="stylesheet" />
+		<link href="/css/fonts.css" rel="stylesheet" />
+	</head>
+	<body>
+		@yield('content')
+	</body>
+</html>
